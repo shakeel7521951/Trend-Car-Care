a6bd693eebe6dff29b025dc3f46462bebc9bca1a
HamzaMajeed86 <rahamza146@gmail.com>
2025-02-07 15:42:12 -0800

    adding swipper in services page

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,8 @@ import Home from "./pages/Home";
 import Services from "./pages/Services";
 import AboutUs from "./pages/AboutUs";
 
-<<<<<<< HEAD
 const MainLayout = () => {
   return (
-=======
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
-import Pricing from './pages/Pricing';
-
-const MainLayout = ()=>{
-  return(
->>>>>>> 83a273e0fdd50fa824946d2f795485fe1042487a
     <>
       <Navbar />
       <Outlet />
@@ -32,7 +18,6 @@ const MainLayout = ()=>{
 
 const router = createBrowserRouter([
   {
-<<<<<<< HEAD
     element: <MainLayout />,
     children: [
       { path: "/", element: <Home /> },
@@ -41,18 +26,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-=======
-    element:<MainLayout />,
-    children:[
-      {path:"/",element:<Home />},
-      {path:"/services",element:<Services />},
-      {path:"/services",element:<Services />},
-      {path:"/about",element:<AboutUs/>},
-      {path:"/pricing",element:<Pricing/>},
-    ]
-  }
-])
->>>>>>> 83a273e0fdd50fa824946d2f795485fe1042487a
 
 function App() {
   return <RouterProvider router={router} />;
