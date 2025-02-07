ba7ffe1355d67e649ad2db3c01ff1061e8115237
asad3473 <asadsoomro0311@gmail.com>
2025-02-07 16:33:55 +0500

    pricenav

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,11 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import AboutUs from "./pages/AboutUs";
-
-<<<<<<< HEAD
-const MainLayout = () => {
-  return (
-=======
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
-import Pricing from './pages/Pricing';
+import Pricing from "./pages/Pricing";
 
 const MainLayout = ()=>{
   return(
->>>>>>> 83a273e0fdd50fa824946d2f795485fe1042487a
+
     <>
       <Navbar />
       <Outlet />
@@ -32,16 +20,6 @@ const MainLayout = ()=>{
 
 const router = createBrowserRouter([
   {
-<<<<<<< HEAD
-    element: <MainLayout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/services", element: <Services /> },
-      { path: "/about", element: <AboutUs /> },
-    ],
-  },
-]);
-=======
     element:<MainLayout />,
     children:[
       {path:"/",element:<Home />},
@@ -52,7 +30,6 @@ const router = createBrowserRouter([
     ]
   }
 ])
->>>>>>> 83a273e0fdd50fa824946d2f795485fe1042487a
 
 function App() {
   return <RouterProvider router={router} />;
